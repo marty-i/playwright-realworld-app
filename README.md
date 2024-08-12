cdcf8eee2724d373ddf1e0e105ceab71d6cb2fae
marty-i <bmn42+github@pm.me>
2024-08-12 11:46:36 +1000

    Add recorded test for home page redirect
    * Update readme with basic test writing process

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ Learning & comparing Playwright automation using https://github.com/cypress-io/c
 Follow the instructions on that page to get setup, then run the app using:
 `yarn dev`
 
-App runs at:
+Cypress Real World App runs at:
 http://localhost:3000
+
+The basic process to creating tests is:
+
+1. Pick a Cypress test to duplicate using Playwright
+2. Record the test using Playwright codegen
+3. Modify and/or clean up the generated code to make it reliable and robust, thereby learning recommended practices along the way
--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -6,3 +6,14 @@ test('should redirect unauthenticated user to signin page', async ({
   await page.goto('http://localhost:3000/personal');
   await expect(page).toHaveURL('http://localhost:3000/signin');
 });
+
+test('should redirect to the home page after login', async ({
+  page,
+}) => {
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('Heath93');
+  await page.getByLabel('Username').press('Tab');
+  await page.getByLabel('Password').fill('s3cret');
+  await page.getByLabel('Remember me').check();
+  await page.locator('[data-test="signin-submit"]').click();
+});
